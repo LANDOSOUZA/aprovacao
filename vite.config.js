dbe150ffcf8406f9462394f07e15253f8c8c6215
Lando Souza <lando-souza@hotmail.com>
2026-04-03 11:45:49 -0300

    Força Vite a copiar a pasta public para o dist

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-//vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  publicDir: 'public', // ✅ FORÇA cópia da pasta public para o dist
   plugins: [
     vue(),
     tailwindcss()
