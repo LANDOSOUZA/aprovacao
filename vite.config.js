1d9bfca9fd19147d5423ca9643ef304e0409b17f
Lando Souza <lando-souza@hotmail.com>
2026-04-04 12:30:27 -0300

    fix: correct @ alias for Vite ESM

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  publicDir: 'public', // ✅ FORÇA cópia da pasta public para o dist
+  publicDir: 'public',
   plugins: [
     vue(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
